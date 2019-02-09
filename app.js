96f795eedede09040b6f527bc4a2c399ba66de94
dugiwarc <gicubotnaru@gmail.com>
2019-02-09 17:38:22 +0100

    remove connection mongo

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var userRoutes      = require('./routes/users'),
 
 var app = express();
 
-mongoose.connect("mongodb://localhost:27017/tusky", {useNewUrlParser:true});
+// mongoose.connect("mongodb://localhost:27017/tusky", {useNewUrlParser:true});
 
 // a line we will see all the time
 app.use(bodyParser.urlencoded({extended: true}));
